4a2dc9e8d3f969184d23827fe3852e562a0e11b3
kubilayeldemir <kubi0707@gmail.com>
2022-04-30 14:13:34 +0300

    Post images to api on selected folder

--- postimagestoapi.py
+++ postimagestoapi.py
@@ -3,6 +3,9 @@ import os
 import base64
 import requests
 
+authToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJlbWFpbCI6InN0cmluYkBhc2QuY29tIiwidXNlcm5hbWUiOiJzdHJpbmciLCJyb2xlIjoiYWRtaW4iLCJuYmYiOjE2NTEyNTk5OTUsImV4cCI6MTY1MTM0NjM5NSwiaWF0IjoxNjUxMjU5OTk1fQ.wuEh0UXYwkHAvrsCmCuLru0lAj9WnLfZaqEHFh8v71M"
+enableRequests = False
+selectedFolder = "images/"
 
 logging.basicConfig(handlers=[
     logging.FileHandler("logs.txt"),
@@ -12,24 +15,43 @@ logging.basicConfig(handlers=[
     format='%(levelname)s: %(asctime)s %(message)s',
     datefmt='%m/%d/%Y %I:%M:%S')
 
-print("sa")
 request_data = []
 
 for root, dirs, files in os.walk("images"):
     i = 0
     for file in files:
         i += 1
-        with open("images/" + file, "rb") as image_file:
+        with open(selectedFolder + file, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read()).decode()
-            image_name = image_file.name
+            image_name = os.path.basename(image_file.name)
+
+            splitImageName = image_name.split('_')
+            imageTitle = splitImageName[0]
+            imageAuthor = splitImageName[1]
+            imageDate = splitImageName[2]
+
             base64_image = 'data:image/png;base64,' + encoded_string
-            print(base64_image)
-            data = {"fileName": image_name, "fileBase64": base64_image}
+            data = {
+                "title": imageTitle,
+                "username": imageAuthor,
+                "fileBase64": base64_image,
+                "timestampString": imageDate
+            }
             request_data.append(data)
-        if i == 10:
-            print(request_data)
-            print("request at")
+        if i / 10 == 1:
+            if enableRequests:
+                response = requests.post(url="http://localhost:5000/api/v1/post/bulk-save-raw", json=request_data,
+                                         headers={"Authorization": authToken}, )
+
             i = 0
+            print(request_data)
             request_data = []
-    print(request_data)
+
+    if len(request_data) > 0:
+        if enableRequests:
+            response = requests.post(url="http://localhost:5000/api/v1/post/bulk-save-raw", json=request_data,
+                                     headers={"Authorization": authToken}, )
+            print(request_data)
+
+
     print("request at")
